f7365426c76a477e5fa6cc60a14ba9b384ff1654
repom2 <mikkorepo@msn.com>
2023-03-15 20:44:03 +0200

    first dbt

--- tennisproject/dbt/tennis_metrics/models/example/my_first_dbt_model.sql
+++ tennisproject/dbt/tennis_metrics/models/example/my_first_dbt_model.sql
@@ -7,18 +7,14 @@
     Try changing "table" to "view" below
 */
 
-{{ config(materialized='table') }}
+{{
+    config(
+    materialized='table'
+    )
+}}
 
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
-)
-
-select *
-from source_data
+select id, slug
+from sportscore_leagues
 
 /*
     Uncomment the line below to remove records with null `id` values
---
+++ tennisproject/tennisapi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TennisapiConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "tennisapi"

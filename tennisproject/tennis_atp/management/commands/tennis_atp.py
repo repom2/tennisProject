349121185c7a257ddcd20350703f3128e60eb6ac
repom2 <mikkorepo@msn.com>
2023-03-28 22:38:13 +0300

    atp matches

--- tennisproject/tennis_atp/management/commands/tennis_atp.py
+++ tennisproject/tennis_atp/management/commands/tennis_atp.py
@@ -7,7 +7,7 @@ import requests
 from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand
 from django.db import transaction
-from tennis_atp.models import Players
+from tennis_atp.models import Players, AtpMatches
 from tqdm import tqdm
 
 pd.set_option('display.max_columns', None)
@@ -24,15 +24,15 @@ class Command(BaseCommand):
         list_players_cmd = subparsers.add_parser("players")
         list_players_cmd.set_defaults(subcommand=self.list_players)
 
+        list_atp_matches_cmd = subparsers.add_parser("atp_matches")
+        list_atp_matches_cmd.set_defaults(subcommand=self.list_atp_matches)
+
     def handle(self, *args, **options):
         options["subcommand"](options)
 
     def list_players(self, options):
-        file_name2 = "/app/tennisproject/tennis_atp/tennis_data/atp_players.csv"
-        file_name = os.path.join(settings.BASE_DIR / "atp_players.csv")
-        print(file_name2)
+        file_name ="/app/tennis_atp/tennis_data/atp_players.csv"
         tmp_data = pd.read_csv(file_name, sep=',')
-
         products = [
             Players(
                 player_id=row['player_id'],
@@ -47,3 +47,65 @@ class Command(BaseCommand):
             for index, row in tmp_data.iterrows()
         ]
         Players.objects.bulk_create(products)
+
+    def list_atp_matches(self, options):
+        start_year = 1968
+        end_year = 2023
+        for i in range(start_year, end_year+1):
+            file_name = "/app/tennis_atp/tennis_data/atp_matches_" + str(i) + ".csv"
+            tmp_data = pd.read_csv(file_name, sep=',')
+            products = [
+                Players(
+                    tourney_id=row['tourney_id'],
+                    tourney_name=row['tourney_name'],
+                    surface=row['surface'],
+                    draw_size=row['draw_size'],
+                    tourney_level=row['tourney_level'],
+                    tourney_date=row['tourney_date'],
+                    match_num=row['match_num'],
+                    winner_id=row['winner_id'],
+                    winner_seed=row['winner_seed'],
+                    winner_entry=row['winner_entry'],
+                    winner_name=row['winner_name'],
+                    winner_hand=row['winner_hand'],
+                    winner_ht=row['winner_ht'],
+                    winner_ioc=row['winner_ioc'],
+                    winner_age=row['winner_age'],
+                    loser_id=row['loser_id'],
+                    loser_seed=row['loser_seed'],
+                    loser_entry=row['loser_entry'],
+                    loser_name=row['loser_name'],
+                    loser_hand=row['loser_hand'],
+                    loser_ht=row['loser_ht'],
+                    loser_ioc=row['loser_ioc'],
+                    loser_age=row['loser_age'],
+                    score=row['score'],
+                    best_of=row['best_of'],
+                    round=row['round'],
+                    minutes=row['minutes'],
+                    w_ace=row['w_ace'],
+                    w_df=row['w_df'],
+                    w_svpt=row['w_svpt'],
+                    w_1stIn=row['w_1stIn'],
+                    w_1stWon=row['w_1stWon'],
+                    w_2ndWon=row['w_2ndWon'],
+                    w_SvGms=row['w_SvGms'],
+                    w_bpSaved=row['w_bpSaved'],
+                    w_bpFaced=row['w_bpFaced'],
+                    l_ace=row['l_ace'],
+                    l_df=row['l_df'],
+                    l_svpt=row['l_svpt'],
+                    l_1stIn=row['l_1stIn'],
+                    l_1stWon=row['l_1stWon'],
+                    l_2ndWon=row['l_2ndWon'],
+                    l_SvGms=row['l_SvGms'],
+                    l_bpSaved=row['l_bpSaved'],
+                    l_bpFaced=row['l_bpFaced'],
+                    winner_rank=row['winner_rank'],
+                    winner_rank_points=row['winner_rank_points'],
+                    loser_rank=row['loser_rank'],
+                    loser_rank_points=row['loser_rank_points'],
+                )
+                for index, row in tmp_data.iterrows()
+            ]
+            AtpMatches.objects.bulk_create(products)

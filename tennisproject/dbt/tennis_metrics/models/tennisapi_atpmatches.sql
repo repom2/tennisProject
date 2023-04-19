68bd6ae2a5bd795eb5da039b0395a1308e9d933d
repom2 <mikkorepo@msn.com>
2023-04-19 22:08:32 +0300

    events

--- tennisproject/dbt/tennis_metrics/models/tennisapi_atpmatches.sql
+++ tennisproject/dbt/tennis_metrics/models/tennisapi_atpmatches.sql
@@ -34,4 +34,21 @@ from (
     from tennis_atp_atpmatches a left join tennisapi_players b on winner_id = b.player_id::text
     left join tennisapi_players c on loser_id = c.player_id::text
     inner join tennisapi_atptour t on a.tourney_id=t.id where date <= '2023-02-27'
+
+    union all
+
+    select
+        a.id as match_num,
+        t.id as tour_id,
+        case when winner_code = '1' then b.id else c.id end winner_id,
+	    case when winner_code = '2' then b.id else c.id end loser_id,
+        date(start_at) as date
+    from sportscore_events a inner join tennisapi_atptour t
+    on t.id=CONCAT(EXTRACT('Year' FROM date(start_at)), '-', a.league_id)
+    left join tennisapi_players b on home_team_id::integer = b.sportscore_id
+    left join tennisapi_players c on away_team_id::integer = c.sportscore_id
+    where start_at::timestamp > '2023-02-27'
+    and sport_id='2'
+    and status = 'finished'
+    and (winner_code = '1' or winner_code = '2')
 ) s

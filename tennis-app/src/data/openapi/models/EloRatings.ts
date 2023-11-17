3ba25e9d631da5671babfe1e171ac5f025624f9d
Mikko Repo <mikko.repo@surveypal.com>
2023-11-17 12:14:08 +0200

    openapi

---
+++ tennis-app/src/data/openapi/models/EloRatings.ts
@@ -0,0 +1,27 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type EloRatings = {
+    /**
+     * Player ID
+     */
+    id?: string;
+    /**
+     * First name
+     */
+    first_name?: string;
+    /**
+     * Last Name
+     */
+    last_name?: string;
+    /**
+     * EloRating
+     */
+    elo?: number;
+    /**
+     * Latest Record Date
+     */
+    latest_date?: number;
+};
+

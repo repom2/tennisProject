aa9df2d843a903985230762771bcd79f801c80b0
Mikko Repo <mikko.repo@surveypal.com>
2024-02-17 10:50:35 +0200

    football bets

---
+++ tennis-app/src/data/openapi/models/FootballBets.ts
@@ -0,0 +1,83 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type FootballBets = {
+    /**
+     * Match ID
+     */
+    matchId?: string;
+    /**
+     * Home Team ID
+     */
+    homeId?: string;
+    /**
+     * Away Team ID
+     */
+    awayId?: string;
+    /**
+     * Home Team Name
+     */
+    homeName?: string;
+    /**
+     * Away Team Name
+     */
+    awayName?: string;
+    /**
+     * Home Odds
+     */
+    homeOdds?: number;
+    /**
+     * Draw Odds
+     */
+    drawOdds?: number;
+    /**
+     * Away Odds
+     */
+    awayOdds?: number;
+    /**
+     * Home Probability
+     */
+    homeProb?: number;
+    /**
+     * Draw Probability
+     */
+    drawProb?: number;
+    /**
+     * Away Probability
+     */
+    awayProb?: number;
+    /**
+     * Home yield
+     */
+    homeYield?: number;
+    /**
+     * Draw yield
+     */
+    drawYield?: number;
+    /**
+     * Away yield
+     */
+    awayYield?: number;
+    /**
+     * Elo Probability
+     */
+    eloProb?: number;
+    /**
+     * Home and Away Elo Probability
+     */
+    eloProbHome?: number;
+    /**
+     * Preview
+     */
+    preview?: string;
+    /**
+     * Reasoning
+     */
+    reasoning?: string;
+    /**
+     * Start time
+     */
+    startAt?: string;
+};
+
